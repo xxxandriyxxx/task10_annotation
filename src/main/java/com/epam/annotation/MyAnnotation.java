9f4a433c8fcfb8f1c99b0740bf27ce284a9c410f
Andriy <andriymattt@gmail.com>
2019-12-05 00:40:49 +0200

    develop: create annotation MyAnnotation

---
+++ src/main/java/com/epam/annotation/MyAnnotation.java
@@ -0,0 +1,16 @@
+package com.epam.annotation;
+
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(value = ElementType.FIELD)
+@Retention(value = RetentionPolicy.RUNTIME)
+
+public @interface MyAnnotation {
+
+    String name() default "DefaultName";
+    int amount() default 10;
+}
