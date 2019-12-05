a918968b6c0fbdc11855f85e6010ef6eb6db7e82
Andriy <andriymattt@gmail.com>
2019-12-05 22:32:17 +0200

    develop: add Model interface

---
+++ src/main/java/com/epam/model/Model.java
@@ -0,0 +1,16 @@
+package com.epam.model;
+
+public interface Model {
+
+    void printAnnotatedFields();
+
+    void testUnknownObj();
+
+    void testGeneric();
+
+    void setValueUnknownType();
+
+    void invokeMethodsDifParam();
+
+    void invokeMyMethod();
+}
