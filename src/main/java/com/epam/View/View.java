8a68588460b5c7463c82691a92bd8f628a798d52
Andriy <andriymattt@gmail.com>
2019-12-05 03:49:00 +0200

    develop: wrote methods in View class

--- src/main/java/com/epam/View/View.java
+++ src/main/java/com/epam/View/View.java
@@ -1,5 +1,12 @@
 package com.epam.View;
 
+import com.epam.Model.MyClass;
+import com.epam.Model.MyGenericClass;
+import com.epam.annotation.MyAnnotation;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Scanner;
@@ -11,41 +18,137 @@ public class View {
     private static Scanner input = new Scanner(System.in);
 
 
-
     public View() {
         menu = new LinkedHashMap<>();
-        menu.put("1", " 1 - ");
-        menu.put("2", " 2 - ");
-        menu.put("3", " 3 - ");
-        menu.put("4", " 4 - ");
-        menu.put("5", " 5 - ");
+        menu.put("1", " 1 - Print fields in MyClass that are annotated by MyAnnotation");
+        menu.put("2", " 2 - Test unknown object");
+        menu.put("3", " 3 - Test generic class");
+        menu.put("4", " 4 - Set value into field not knowing its type");
+        menu.put("5", " 5 - Invoke method (three method with different parameters and return types)");
+        menu.put("6", " 6 - Invoke myMethod(String a, int ... args) and myMethod(String … args)");
+
         menu.put("Q", " q - exit");
         methodsMenu = new LinkedHashMap<>();
-        methodsMenu.put("1", this::pressButton1);
-        methodsMenu.put("2", this::pressButton2);
-        methodsMenu.put("3", this::pressButton3);
-        methodsMenu.put("4", this::pressButton4);
-        methodsMenu.put("5", this::pressButton5);
-    }
-
-    private void pressButton1() {
+        methodsMenu.put("1", this::printAnnotatedFields);
+        methodsMenu.put("2", this::testUnknownObj);
+        methodsMenu.put("3", this::testGeneric);
+        methodsMenu.put("4", this::setValueUnknownType);
+        methodsMenu.put("5", this::invokeMethodsDifParam);
+        methodsMenu.put("6", this::invokeMyMethod);
 
     }
 
-    private void pressButton2() {
+    private void printAnnotatedFields() {
+        Class clazz = MyClass.class;
+        Field[] fields = clazz.getDeclaredFields();
+        for (Field f : fields) {
+            if (f.isAnnotationPresent(MyAnnotation.class)) {
+                MyAnnotation myAnnotation = (MyAnnotation) f.getAnnotation(MyAnnotation.class);
+                String name = myAnnotation.name();
+                int amount = myAnnotation.amount();
+                System.out.println("-----------");
+                System.out.println("field: " + f.getName());
+                System.out.println("  @MyAnnotation(name = " + name + ")");
+                System.out.println("  @MyAnnotation(amount = " + amount + ")");
 
+            }
+        }
     }
 
-    private void pressButton3() {
+    private void testUnknownObj() {
+        MyClass object = new MyClass();
+        try {
+            Class clazz = object.getClass();
+            Constructor constructor = clazz.getConstructor();
+            Method[] methods = clazz.getMethods();
+            Field[] fields = clazz.getDeclaredFields();
+            System.out.println("-----------");
+            System.out.println("Simple name of class: " + clazz.getSimpleName());
+            System.out.println("Name of class: " + clazz.getName());
+            System.out.println("-----------");
+            System.out.println("Name of constructor: " + constructor);
+            System.out.println("-----------");
+            System.out.println("ALL methods of class:");
+            for (Method m : methods) {
+                System.out.println("- " + m.getName());
+            }
+            System.out.println("-----------");
+            System.out.println("DECLARED fields of class:");
+            for (Field f : fields) {
+                System.out.println("- " + f.getName());
+            }
 
+            fields[1].setAccessible(true);
+            if (fields[1].getType() == int.class) {
+                fields[1].setInt(object, 1000);
+            }
+            System.out.println("-----------");
+            System.out.println("Set fields[1] of object - (fields[1].setAccessible(true)):");
+            System.out.println(object);
+
+            System.out.println("-----------");
+            System.out.println("Invoke private method getInfo():");
+            Method method = clazz.getDeclaredMethod("getInfo");
+            method.setAccessible(true);
+            method.invoke(object);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
-    private void pressButton4() {
-
+    private void testGeneric() {
+        MyGenericClass<MyClass> obj = new MyGenericClass<>(MyClass.class);
     }
 
-    private void pressButton5() {
+    private void setValueUnknownType() {
+    }
 
+    private void invokeMethodsDifParam() {
+        MyClass myClass = new MyClass();
+        Class clazz = myClass.getClass();
+        try {
+            Method method1 = clazz.getDeclaredMethod("method1");
+            method1.setAccessible(true);
+            method1.invoke(myClass);
+
+            Method method2 = clazz.getDeclaredMethod("method2", String.class);
+            method2.setAccessible(true);
+            String str = (String) method2.invoke(myClass, "Call method2 --> ");
+            System.out.println(str);
+
+            Method method3 = clazz.getDeclaredMethod("method3");
+            method3.setAccessible(true);
+            int number = (Integer) method3.invoke(myClass);
+            System.out.println("Call method3 --> " + number);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void invokeMyMethod() {
+        MyClass myClass = new MyClass();
+        Class clazz = myClass.getClass();
+        String[] strs = {"one", "two", "three"};
+        try {
+            Method method1 = clazz.getDeclaredMethod("myMethod", new Class[]{String.class, String[].class});
+            method1.setAccessible(true);
+            String[] strs2 = (String[]) method1.invoke(myClass, "Hello", strs);
+            System.out.println("-----------");
+            for (String s : strs2) {
+                System.out.println(s);
+            }
+
+            Method method2 = clazz.getDeclaredMethod("myMethod", String[].class);
+            method2.setAccessible(true);
+            String[] strs3 = (String[]) method2.invoke(myClass, new Object[]{strs});
+            System.out.println("-----------");
+            for (String s : strs3) {
+                System.out.println(s);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     private void outputMenu() {
